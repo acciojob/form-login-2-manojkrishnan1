e11ec83189ad82abfa693c54bd4bff3e95817510
manojkrishnan1 <127621202+manojkrishnan1@users.noreply.github.com>
2023-11-17 17:32:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function display(){
+  const fname = document.getElementById("fname");
+  const lname = document.getElementById("lname");
+  const phone = document.getElementById("phone");
+  const email = document.getElementById("email");
+  alert(fname.value + "\n" + lname.value +"\n" + phone.value + "\n" + email.value);
+}
